bdd6da11fbb697a9da49e07fd323baed51abd91f
scullionw <scuw1801@usherbrooke.ca>
2019-01-01 01:31:19 -0500

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ fn main() {
     stack.insert_front(6.9);
 
     while let Some(val) = stack.remove_front() {
-         println!("Value popped: {:?}", val);
+        println!("Value popped: {:?}", val);
     }
-
 }
 
 struct Node<T> {
@@ -16,28 +15,30 @@ struct Node<T> {
 
 struct LinkedList<T> {
     head: Option<Box<Node<T>>>,
-    len: usize
+    len: usize,
 }
 
 impl<T> LinkedList<T> {
     fn new() -> LinkedList<T> {
-        LinkedList {
-            head: None,
-            len: 0
-        }
+        LinkedList { head: None, len: 0 }
     }
     fn is_empty(&self) -> bool {
-        return self.len == 0
+        return self.len == 0;
     }
 
     fn insert_front(&mut self, val: T) {
         if self.is_empty() {
-            let new_head = Node { data: val, next: None };
+            let new_head = Node {
+                data: val,
+                next: None,
+            };
             self.head = Some(Box::new(new_head));
-            
         } else {
             let current_head = self.head.take();
-            let new_head = Node { data: val, next: current_head };
+            let new_head = Node {
+                data: val,
+                next: current_head,
+            };
             self.head = Some(Box::new(new_head));
         }
         self.len += 1;
@@ -45,13 +46,15 @@ impl<T> LinkedList<T> {
 
     fn remove_front(&mut self) -> Option<T> {
         if self.is_empty() {
-            None  
+            None
         } else {
-            let removed_head = self.head.take().expect("Not empty so head should not be None.");
+            let removed_head = self
+                .head
+                .take()
+                .expect("Not empty so head should not be None.");
             self.head = removed_head.next;
             self.len -= 1;
             Some(removed_head.data)
         }
     }
 }
-

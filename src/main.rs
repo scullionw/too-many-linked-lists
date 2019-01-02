2c964fbb51c39a149f7cc0ef9035fadbd9de3808
scullionw <scuw1801@usherbrooke.ca>
2019-01-01 19:40:28 -0500

    add tests and use match instead of len

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
     let mut stack = LinkedList::new();
-    stack.insert_front(5.0);
-    stack.insert_front(6.9);
-
+    (0..100).for_each(|val| stack.insert_front(val));
     while let Some(val) = stack.remove_front() {
         println!("Value popped: {:?}", val);
     }
@@ -22,39 +20,56 @@ impl<T> LinkedList<T> {
     fn new() -> LinkedList<T> {
         LinkedList { head: None, len: 0 }
     }
-    fn is_empty(&self) -> bool {
-        self.len == 0
-    }
 
     fn insert_front(&mut self, val: T) {
-        if self.is_empty() {
-            let new_head = Node {
-                data: val,
-                next: None,
-            };
-            self.head = Some(Box::new(new_head));
-        } else {
-            let current_head = self.head.take();
-            let new_head = Node {
-                data: val,
-                next: current_head,
-            };
-            self.head = Some(Box::new(new_head));
+        match self.head {
+            Some(_) => {
+                let new_head = Node {
+                    data: val,
+                    next: self.head.take(),
+                };
+                self.head = Some(Box::new(new_head));
+            }
+            None => {
+                let new_head = Node {
+                    data: val,
+                    next: None,
+                };
+                self.head = Some(Box::new(new_head));
+            }
         }
         self.len += 1;
     }
 
     fn remove_front(&mut self) -> Option<T> {
-        if self.is_empty() {
-            None
-        } else {
-            let removed_head = self
-                .head
-                .take()
-                .expect("Not empty so head should not be None.");
-            self.head = removed_head.next;
-            self.len -= 1;
-            Some(removed_head.data)
+        match self.head {
+            Some(_) => {
+                let removed_head = self
+                    .head
+                    .take()
+                    .expect("Not empty so head should not be None.");
+                self.head = removed_head.next;
+                self.len -= 1;
+                Some(removed_head.data)
+            }
+            None => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_push_pop() {
+        let mut stack = LinkedList::new();
+        assert_eq!(stack.remove_front(), None);
+        assert_eq!(stack.remove_front(), None);
+        stack.insert_front(1);
+        stack.insert_front(2);
+        assert_eq!(stack.remove_front(), Some(2));
+        assert_eq!(stack.remove_front(), Some(1));
+        assert_eq!(stack.remove_front(), None);
+        assert_eq!(stack.remove_front(), None);
     }
 }
